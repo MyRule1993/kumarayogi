dac26e86f955a74bad4b3f5299ab0e0ccee1fb6a
Ppraveengouda <patilpraveen42@gmail.com>
2017-03-04 19:10:34 +0530

    first commit

--- app/src/main/java/com/myrule/kumarayogi/Home.java
+++ app/src/main/java/com/myrule/kumarayogi/Home.java
@@ -1,8 +1,6 @@
 package com.myrule.kumarayogi;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
